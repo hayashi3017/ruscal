11bb47887b664b04b279073043d12ab0a5aa7fd1
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-08-13 19:32:55 +0900

    3.3 トークンの切り出し

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Ident,
+    Number,
+}
+
 fn main() {
-    let source = "123 world";
-    println!(
-        "source: {}, parsed: {:?}",
-        source,
-        ident(whitespace(source))
-    )
+    let input = "123 world";
+    println!("source: {}, parsed: {:?}", input, source(input));
+    let input = "Hello world";
+    println!("source: {}, parsed: {:?}", input, source(input));
+    let input = "     world";
+    println!("source: {}, parsed: {:?}", input, source(input));
+}
+
+fn source(mut input: &str) -> Vec<Token> {
+    let mut tokens = vec![];
+    while !input.is_empty() {
+        input = if let (next_input, Some(token)) = token(input) {
+            tokens.push(token);
+            next_input
+        } else {
+            break;
+        }
+    }
+    tokens
+}
+
+fn token(i: &str) -> (&str, Option<Token>) {
+    if let (i, Some(ident_res)) = ident(whitespace(i)) {
+        return (i, Some(ident_res));
+    }
+
+    if let (i, Some(number_res)) = number(whitespace(i)) {
+        return (i, Some(number_res));
+    }
+
+    (i, None)
 }
 
 fn whitespace(mut input: &str) -> &str {
@@ -16,7 +47,7 @@ fn whitespace(mut input: &str) -> &str {
     input
 }
 
-fn ident(mut input: &str) -> &str {
+fn ident(mut input: &str) -> (&str, Option<Token>) {
     if matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
         while matches!(
             input.chars().next(),
@@ -26,11 +57,13 @@ fn ident(mut input: &str) -> &str {
             chars.next();
             input = chars.as_str();
         }
+        (input, Some(Token::Ident))
+    } else {
+        (input, None)
     }
-    input
 }
 
-fn number(mut input: &str) -> &str {
+fn number(mut input: &str) -> (&str, Option<Token>) {
     if matches!(
         input.chars().next(),
         Some(_x @ ('-' | '+' | '.' | '0'..='9'))
@@ -40,8 +73,10 @@ fn number(mut input: &str) -> &str {
             chars.next();
             input = chars.as_str();
         }
+        (input, Some(Token::Number))
+    } else {
+        (input, None)
     }
-    input
 }
 
 #[cfg(test)]
@@ -55,11 +90,11 @@ mod test {
 
     #[test]
     fn test_ident() {
-        assert_eq!(ident("Adam"), "");
+        assert_eq!(ident("Adam"), ("", Some(Token::Ident)));
     }
 
     #[test]
     fn test_number() {
-        assert_eq!(number("123.45 "), " ");
+        assert_eq!(number("123.45 "), (" ", Some(Token::Number)));
     }
 }
